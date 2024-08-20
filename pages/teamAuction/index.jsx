e227ae8578acaf5ca02909f59f7b961d54266952
tuosm9390 <tuosm123@naver.com>
2024-08-20 18:37:56 +0900

    hidration 처리

--- pages/teamAuction/index.jsx
+++ pages/teamAuction/index.jsx
@@ -38,74 +38,81 @@ const imageMap = {
 
 export default function index() {
   return (
-    <div className={styles.container}>
-      <h1>리그전 팀 경매</h1>
-      <section className={styles.content_container}>
-        <div className={styles.auctionBox_container}>
-          <div className={`${styles.auction_box} ${styles.team_list}`}>
-            <h2 className={styles.team_list_title}>팀 리스트</h2>
-            {teamList.map((item, index) => (
-              <div key={index} className={styles.team_box}>
-                <h3 className={styles.team_name}>{item.team_name}</h3>
-                <div className={styles.member}>
-                  <Image src={topLane} alt="topLane" width={25} />
+    teamList &&
+    memberList && (
+      <div className={styles.container}>
+        <h1>리그전 팀 경매</h1>
+        <section className={styles.content_container}>
+          <div className={styles.auctionBox_container}>
+            <div className={`${styles.auction_box} ${styles.team_list}`}>
+              <h2 className={styles.team_list_title}>팀 리스트</h2>
+              {teamList.map((item, index) => (
+                <div key={index} className={styles.team_box}>
+                  <h3 className={styles.team_name}>{item.team_name}</h3>
+                  <div className={styles.member}>
+                    <Image src={topLane} alt="topLane" width={25} />
+                  </div>
+                  <div className={styles.member}>
+                    <Image src={jgLane} alt="jgLane" width={25} />
+                  </div>
+                  <div className={styles.member}>
+                    <Image src={midLane} alt="midLane" width={25} />
+                  </div>
+                  <div className={styles.member}>
+                    <Image src={adLane} alt="adLane" width={25} />
+                  </div>
+                  <div className={styles.member}>
+                    <Image src={supLane} alt="supLane" width={25} />
+                  </div>
                 </div>
-                <div className={styles.member}>
-                  <Image src={jgLane} alt="jgLane" width={25} />
-                </div>
-                <div className={styles.member}>
-                  <Image src={midLane} alt="midLane" width={25} />
-                </div>
-                <div className={styles.member}>
-                  <Image src={adLane} alt="adLane" width={25} />
-                </div>
-                <div className={styles.member}>
-                  <Image src={supLane} alt="supLane" width={25} />
-                </div>
+              ))}
+            </div>
+            <div className={`${styles.auction_box} ${styles.live_member}`}>
+              <h2>현재 인원</h2>
+              <div className={styles.live_member_box}>
+                <Image
+                  src={imageMap[isLiveMember.position] || ""}
+                  alt="/"
+                  width={300}
+                />
               </div>
-            ))}
-          </div>
-          <div className={`${styles.auction_box} ${styles.live_member}`}>
-            <h2>현재 인원</h2>
-            <div className={styles.live_member_box}>
-              <Image
-                src={imageMap[isLiveMember.position] || ""}
-                alt="/"
-                width={300}
-              />
-            </div>
-            <div className={styles.member_info}>
-              <h3>이름</h3>
-              <h3>{isLiveMember.member_name}</h3>
-              <h3>티어</h3>
-              <h3>{isLiveMember.tier}</h3>
-              <h3>라인</h3>
-              <h3>{isLiveMember.position}</h3>
-              <h3>각오</h3>
-              <h3>{isLiveMember.description}</h3>
-            </div>
+              <div className={styles.member_info}>
+                <h3>이름</h3>
+                <h3>{isLiveMember.member_name}</h3>
+                <h3>티어</h3>
+                <h3>{isLiveMember.tier}</h3>
+                <h3>라인</h3>
+                <h3>{isLiveMember.position}</h3>
+                <h3>각오</h3>
+                <h3>{isLiveMember.description}</h3>
+              </div>
 
-            <Timer className={styles.timer} />
-          </div>
-          <div className={`${styles.auction_box} ${styles.waiting_list}`}>
-            <h2 className={styles.waiting_list_title}>대기명단</h2>
-            {processMembers(memberList).map((item, index) => (
-              <div key={index} className={styles.waiting_member_list}>
-                <Image src={imageMap[item.position] || ""} alt="/" width={60} />
-                <div className={styles.waiting_member_list_name}>
-                  <span>{item.member_name}</span>
+              <Timer className={styles.timer} />
+            </div>
+            <div className={`${styles.auction_box} ${styles.waiting_list}`}>
+              <h2 className={styles.waiting_list_title}>대기명단</h2>
+              {processMembers(memberList).map((item, index) => (
+                <div key={index} className={styles.waiting_member_list}>
                   <Image
                     src={imageMap[item.position] || ""}
                     alt="/"
-                    width={15}
+                    width={60}
                   />
+                  <div className={styles.waiting_member_list_name}>
+                    <span>{item.member_name}</span>
+                    <Image
+                      src={imageMap[item.position] || ""}
+                      alt="/"
+                      width={15}
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    )
   );
 }
 
