9a603ab677a04bdb03aac0ffc39ee3119fb2c45e
tuosm9390 <tuosm123@naver.com>
2024-08-23 18:37:47 +0900

    경매 낙찰 시스템 작업중, 정보변경시 실시간 반영 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
--- pages/teamAuction/index.jsx
+++ pages/teamAuction/index.jsx
@@ -8,123 +8,198 @@ import supLane from "../../public/supLane.png";
 import topLane from "../../public/topLane.png";
 import styles from "./teamAuction.module.css";
 import { supabase } from "@/supabase.config";
+import { useState, useEffect } from "react";
 
-// supabase의 query문을 사용해서 조회
-const { data: teamList } = await supabase.rpc("team_list");
-// team_name이 null인 경우만 조회 : 팀에 소속되어 있지 않음.
-const { data: memberList } = await supabase.from("minions_member").select().is("team_name", null);
+export default function Index() {
+  const [teamList, setTeamList] = useState();
+  const [memberList, setMemberList] = useState();
+  const [isLiveMember, setIsLiveMember] = useState();
+  const [bidTeam, setBidTeam] = useState({
+    id: 1,
+    team_name: "팀1",
+    initialPoint: 1000,
+    bidPoint: 100,
+  });
 
-// 회원 목록을 필터링하고 정렬하는 함수
-const processMembers = (members) => {
-  return members
-    .filter((member) => member.isLive === false) // isLive가 false인 항목만 필터링
-    .slice() // 원본 배열을 변형하지 않기 위해 복사
-    .sort((a, b) => {
-      // `number` 값이 있는 항목을 기준으로 정렬
-      const numberA = a.number !== null ? a.number : Number.MAX_VALUE; // null인 경우 뒤로 배치
-      const numberB = b.number !== null ? b.number : Number.MAX_VALUE;
+  const fetchTeamList = async () => {
+    const { data, error } = await supabase.rpc("team_list");
+    if (error) {
+      console.error("Error fetching team list:", error);
+    } else {
+      setTeamList(data);
+    }
+  };
 
-      return numberA - numberB;
-    });
-};
+  const fetchMemberList = async () => {
+    const { data, error } = await supabase
+      .from("minions_member")
+      .select()
+      .is("team_id", null);
+    if (error) {
+      console.error("Error fetching member list:", error);
+    } else {
+      setMemberList(data);
+      const isLiveMember = data.filter((member) => member.isLive === true)[0];
+      setIsLiveMember(isLiveMember);
+    }
+  };
 
-const isLiveMember = memberList.filter((member) => member.isLive === true)[0];
+  // 회원 목록을 필터링하고 정렬하는 함수
+  const processMembers = (members) => {
+    return members
+      ?.filter((member) => member.isLive === false) // isLive가 false인 항목만 필터링
+      .slice() // 원본 배열을 변형하지 않기 위해 복사
+      .sort((a, b) => {
+        // `number` 값이 있는 항목을 기준으로 정렬
+        const numberA = a.number !== null ? a.number : Number.MAX_VALUE; // null인 경우 뒤로 배치
+        const numberB = b.number !== null ? b.number : Number.MAX_VALUE;
 
-const imageMap = {
-  top: topLane,
-  jg: jgLane,
-  mid: midLane,
-  ad: adLane,
-  sup: supLane,
-};
+        return numberA - numberB;
+      });
+  };
 
-export default function index() {
+  // 입찰 진행
+  const placeBid = () => {};
+
+  // 낙찰
+  const successfulBid = async () => {
+    const responseMember = await supabase
+      .from("minions_member")
+      .update({
+        team_id: bidTeam.id,
+        team_name: bidTeam.team_name,
+        point: bidTeam.bidPoint,
+      })
+      .eq("id", isLiveMember.id)
+      .select();
+
+    const responseTeam = await supabase
+      .from("minions_team")
+      .update({
+        [isLiveMember.position]: isLiveMember.id,
+        point: bidTeam.initialPoint - bidTeam.bidPoint,
+      })
+      .eq("id", isLiveMember.id)
+      .select();
+  };
+
+  const imageMap = {
+    top: topLane,
+    jg: jgLane,
+    mid: midLane,
+    ad: adLane,
+    sup: supLane,
+  };
+
+  useEffect(() => {
+    // 채널 구독
+    const table_update_channel = supabase
+      .channel("table-update-channel")
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "*" },
+        async (payload) => {
+          console.log("Member change received!", payload);
+          await fetchTeamList(); // 팀 리스트 재조회
+          await fetchMemberList(); // 멤버 리스트 재조회
+        }
+      )
+      .subscribe();
+
+    // unsubscribe 함수를 반환합니다.
+    return () => {
+      table_update_channel.unsubscribe();
+    };
+  }, []);
+
+  // 컴포넌트 렌더링 시 초기 데이터 로드
+  useEffect(() => {
+    fetchTeamList();
+    fetchMemberList();
+  }, []);
+
   return (
-    teamList &&
-    memberList && (
-      <div className={styles.container}>
-        <h1>리그전 팀 경매</h1>
-        <section className={styles.content_container}>
-          <div className={styles.auctionBox_container}>
-            <div className={`${styles.auction_box} ${styles.team_list}`}>
-              <h2 className={styles.team_list_title}>팀 리스트</h2>
-              {teamList.map((item, index) => (
-                <div key={index} className={styles.team_box}>
-                  <h3 className={styles.team_name}>{item.team_name}</h3>
-                  <div className={styles.member}>
-                    <Image src={topLane} alt="topLane" width={25} />
-                    <span>{item.top_member_name}</span>
-                  </div>
-                  <div className={styles.member}>
-                    <Image src={jgLane} alt="jgLane" width={25} />
-                    <span>{item.jg_member_name}</span>
-                  </div>
-                  <div className={styles.member}>
-                    <Image src={midLane} alt="midLane" width={25} />
-                    <span>{item.mid_member_name}</span>
-                  </div>
-                  <div className={styles.member}>
-                    <Image src={adLane} alt="adLane" width={25} />
-                    <span>{item.ad_member_name}</span>
-                  </div>
-                  <div className={styles.member}>
-                    <Image src={supLane} alt="supLane" width={25} />
-                    <span>{item.sup_member_name}</span>
-                  </div>
+    <div className={styles.container}>
+      <h1>리그전 팀 경매</h1>
+      <section className={styles.content_container}>
+        <div className={styles.auctionBox_container}>
+          <div className={`${styles.auction_box} ${styles.team_list}`}>
+            <h2 className={styles.team_list_title}>팀 리스트</h2>
+            {teamList?.map((item, index) => (
+              <div key={index} className={styles.team_box}>
+                <h3 className={styles.team_name}>{item.team_name}</h3>
+                <div className={styles.member}>
+                  <Image src={topLane} alt="topLane" width={25} />
+                  <span>{item.top_member_name}</span>
                 </div>
-              ))}
+                <div className={styles.member}>
+                  <Image src={jgLane} alt="jgLane" width={25} />
+                  <span>{item.jg_member_name}</span>
+                </div>
+                <div className={styles.member}>
+                  <Image src={midLane} alt="midLane" width={25} />
+                  <span>{item.mid_member_name}</span>
+                </div>
+                <div className={styles.member}>
+                  <Image src={adLane} alt="adLane" width={25} />
+                  <span>{item.ad_member_name}</span>
+                </div>
+                <div className={styles.member}>
+                  <Image src={supLane} alt="supLane" width={25} />
+                  <span>{item.sup_member_name}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className={`${styles.auction_box} ${styles.live_member}`}>
+            <h2>현재 인원</h2>
+            <div className={styles.live_member_box}>
+              <Image
+                src={imageMap[isLiveMember?.position] || ""}
+                alt="/"
+                width={300}
+              />
             </div>
-            <div className={`${styles.auction_box} ${styles.live_member}`}>
-              <h2>현재 인원</h2>
-              <div className={styles.live_member_box}>
-                <Image
-                  src={imageMap[isLiveMember.position] || ""}
-                  alt="/"
-                  width={300}
-                />
-              </div>
-              <div className={styles.member_info}>
-                <h3>이름</h3>
-                <h3>{isLiveMember.member_name}</h3>
-                <h3>아이디</h3>
-                <h3>{isLiveMember.in_game_id}</h3>
-                <h3>티어</h3>
-                <h3>{isLiveMember.tier}</h3>
-                <h3>라인</h3>
-                <h3>{isLiveMember.position}</h3>
-                <h3>각오</h3>
-                <h3>{isLiveMember.description}</h3>
-              </div>
+            <div className={styles.member_info}>
+              <h3>이름</h3>
+              <h3>{isLiveMember?.member_name}</h3>
+              <h3>아이디</h3>
+              <h3>{isLiveMember?.in_game_id}</h3>
+              <h3>티어</h3>
+              <h3>{isLiveMember?.tier}</h3>
+              <h3>라인</h3>
+              <h3>{isLiveMember?.position}</h3>
+              <h3>각오</h3>
+              <h3>{isLiveMember?.description}</h3>
+            </div>
 
-              <Timer className={styles.timer} />
-            </div>
-            <div className={`${styles.auction_box} ${styles.waiting_list}`}>
-              <h2 className={styles.waiting_list_title}>대기명단</h2>
-              {processMembers(memberList).map((item, index) => (
-                <div key={index} className={styles.waiting_member_list}>
+            <Timer className={styles.timer} />
+            <button type="button" onClick={() => successfulBid()}>
+              낙찰
+            </button>
+          </div>
+          <div className={`${styles.auction_box} ${styles.waiting_list}`}>
+            <h2 className={styles.waiting_list_title}>대기명단</h2>
+            {processMembers(memberList)?.map((item, index) => (
+              <div key={index} className={styles.waiting_member_list}>
+                <Image src={imageMap[item.position] || ""} alt="/" width={60} />
+                <div className={styles.waiting_member_list_name}>
+                  <span>{item.in_game_id}</span>
                   <Image
                     src={imageMap[item.position] || ""}
                     alt="/"
-                    width={60}
+                    width={15}
                   />
-                  <div className={styles.waiting_member_list_name}>
-                    <span>{item.in_game_id}</span>
-                    <Image
-                      src={imageMap[item.position] || ""}
-                      alt="/"
-                      width={15}
-                    />
-                  </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-        </section>
-      </div>
-    )
+        </div>
+      </section>
+    </div>
   );
 }
 
-index.getLayout = function getLayout(page) {
+Index.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
